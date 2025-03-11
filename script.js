d2d09121fa25f31defcb5ea544412cea0cfeaadf
Johan Silva Cardoso <142803636+johanssantos02@users.noreply.github.com>
2025-03-10 21:23:48 -0300

    atualização dia 10

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 $(function () {
   $('.imagens').slick({
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
+    slidesToScroll: 2,
     asNavFor: '.imagens',
-    button: false,
+    infinite: true,
   });
   $('.img-nav').slick({
-    slidesToShow: 5,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 1,
     asNavFor: '.imagens',
-    centerMode: true,
+    centerMode: false,
+    centerPadding: '100px',
     focusOnSelect: true,
     adaptiveHeight: true,
-    autoplay: true,
     prevArrow: false,
     nextArrow: false,
+    autoplay: true,
     autoplaySpeed: 2000,
+    infinite: true,
+    
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+
   });
 });
 
 var botao = document.querySelector('input[type=button]');
 
-botao.addEventListener('click', ()=>{
+botao.addEventListener('click', () => {
   alert('Mensagem enviada com sucesso!')
 })
